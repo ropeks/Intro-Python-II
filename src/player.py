725acd9b8acb7edbfee443e3f8e8c7f4950d2d3d
ropeks <ropeksmilioner@gmail.com>
2020-02-12 15:06:58 +0100

    move rooms to player.py and add 'walking' functionality

--- src/player.py
+++ src/player.py
@@ -1,5 +1,16 @@
+from room import Room
+
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+room = {
+    'outside': Room('outside', 'nothing', 'foyer', 'nothing', 'nothing'),
+    'narrow': Room('narrow', 'nothing', 'treasure', 'foyer', 'nothing'),
+    'foyer': Room('foyer', 'outside', 'overlook', 'nothing', 'narrow'),
+    'treasure': Room('treasure', 'narrow', 'nothing', 'overlook', 'nothing'),
+    'overlook': Room('overlook', 'foyer', 'nothing', 'nothing', 'treasure')
+}
+
 class Player:
     def __init__(self, room):
         self.room = room
@@ -8,14 +19,34 @@ class Player:
         return f'You are currently in room {self.room}'
 
     def go_north(self):
-        print('-> going north!\n\n')
+        north = room[self.room].northworld
+        if north != 'nothing':
+            print('-> going north!\n\n')
+            self.room = north
+        else:
+            print('-> nothing there..\n\n')
 
     def go_south(self):
-        print('-> going south!\n\n')
+        south = room[self.room].southworld
+        if south != 'nothing':
+            print('-> going south!\n\n')
+            self.room = south
+        else:
+            print('-> nothing there..\n\n')
 
     def go_east(self):
-        print('-> going east!\n\n')
+        east = room[self.room].eastworld
+        if east != 'nothing':
+            print('-> going east!\n\n')
+            self.room = east
+        else:
+            print('-> nothing there..\n\n')
 
     def go_west(self):
-        print('-> going west!\n\n')
+        west = room[self.room].westworld
+        if west != 'nothing':
+            print('-> going west!\n\n')
+            self.room = west
+        else:
+            print('-> nothing there..\n\n')
 
